0a062fb3dc7e8653e4adede53a1c8a905c39945b
dmytrenkojulia <dmytrenkojulia@gmail.com>
2021-02-06 23:10:14 +0100

    style: strap with buttons

--- pages/beverages/[id].tsx
+++ pages/beverages/[id].tsx
@@ -8,7 +8,9 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import {beverage} from "../../lib/myTypes";
 import BorderUI from "../../components/UI/border";
-
+import clsx from "clsx";
+import {Button} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
 
 export async function getStaticProps({params}) {
     const beverage = await getBeverage(params.id)
@@ -65,7 +67,8 @@ const useStyles = makeStyles((theme) => ({
         width: "auto",
         height: "auto",
         // backgroundColor: theme.palette.background.default,
-
+    },
+    background: {
         border: `2px outset ${theme.palette.secondary.main}`,
         backgroundColor: theme.palette.secondary.main,
     },
@@ -92,6 +95,22 @@ const useStyles = makeStyles((theme) => ({
         display: `block`,
         margin: `0 auto`,
     },
+    strapWithButtons: {
+        marginTop: `${theme.spacing(5)}px`,
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "1rem",
+        padding: `${theme.spacing(1)}px`,
+        [theme.breakpoints.up('md')]: {
+            marginTop: 0,
+        },
+    },
+    addToMyList: {
+        flex: 1,
+    },
+    otherProductsButton: {
+
+    }
 }));
 
 
@@ -111,30 +130,35 @@ export default function Beverage({beverage: beverage}) {
 
 
             <Grid container className={classes.root} spacing={2}>
-                    <Grid item xs={12} lg={3}>
+                    <Grid item xs={12} md={4} lg={3}>
                         <BorderUI>
                             <img className={classes.img} src={beverage.img} alt={beverage.name}/>
                         </BorderUI>
                     </Grid>
-                <Grid item xs={12} lg={9}>
+                <Grid item xs={12} md={8} lg={9}>
                     <BorderUI className={classes.desc}>
                         <div>
                             <h2>{beverage.name}</h2>
                             <p>{beverage.description}</p>
                         </div>
-                        <div className={classes.price}><Typography variant="h5" component="span" align="center">CENA</Typography></div>
+                        <div className={classes.price}><Typography variant="h5" component="span" align="center">9,99zł</Typography></div>
                     </BorderUI>
-                    <div className={classes.producer}>
+                    <div className={clsx(classes.producer,classes.background)}>
                        <Typography variant="h5" component="p" >Producent: jakisproducent</Typography>
                     </div>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <BorderUI>
-                    pasek inne produkty etc.
-                        zmienić wygląd bordera tego paska
-                        może być taka wstążka css
-                    </BorderUI>
+                    <div className={clsx(classes.strapWithButtons,classes.background)}>
+                        <div className={classes.addToMyList}>
+                            <Button variant="contained" color="primary"  startIcon={ <AddIcon/>}>
+                               Dodaj do mojej listy
+                            </Button>
+                        </div>
+                        <Button className={classes.otherProductsButton} variant="contained" color="primary">
+                            Inne produkty tego producenta
+                        </Button>
+                    </div>
                 </Grid>
 
                 <Grid item xs={12} lg={3}>
